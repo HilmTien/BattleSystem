9fee832c929db1787fffd61c7248ff48f7aeea1e
Tien <53821597+HilmTien@users.noreply.github.com>
2021-02-09 22:07:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
+
 # Programmeringsvurdering våren 2021
 
 # Enkelt battle system utviklet av Tien og Vincent (2021)
 
 import console
-
-console.clear()
-print('hello, world!')
-
-a = 20
-
-if a == 10:
-	print("works")
-
-for i in range(10):
-	print(i)
-
-FargeListe = ['red','orange','yellow','green','cyan','blue','violet']
-def rainbow(Farger):
-	for i in Farger:
-		print(i)
-
-
-rainbow(FargeListe)
-
